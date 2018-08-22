6158d4a3e9ff2d3a85c58a45be33d57bead128c3
yohan1406 <42361586+yohan1406@users.noreply.github.com>
2018-08-21 20:30:41 -0500

    Add files via upload

--- main.c
+++ main.c
@@ -1,34 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define PI 3.1416
-const float g=9.81;
-float H= 12;
-int variable;
-void funcion(){
-static int cuenta=0; //static cuenta como asociar grupo como en este caso junta cuenta como global
-cuenta=cuenta+1;
-variable=11;
-printf("variable=%d\n",variable);
-printf("funcion():%f\ncuenta=|%-5d|\n",H,cuenta);     
+#define max(a,b) (a>=b)? a:b
+int absoluto(int x){
+  return (x>0)? x:-x;
+  
 }
+
 int main(int argc, char *argv[])
 {
-  float area;
-  float radio;
-  funcion();
-  dummy();
-  printf("H=%f\nIngrese el valor del radio de una\
- circunferencia:"),H;
-  H=12.1;
-  printf("H=|%-10.6f|\n",H);
-  funcion();
-  variable=11;
-  printf("variable=%d\n",variable);
-  //scanf("%f",&radio);
-  
-  //printf("El \'area de la circunferencia es: %f\n\n",
-    //      PI*radio*radio);
-  
+int A,B,R; A=-10; B=725; 
+A=absoluto(A); 
+B=absoluto(B);
+  printf("%d\t%d\n",A,B);
+  R= A;
+  A=max(A,B); B= (A-R!=0)?R:B;
+  printf("%d\t%d\n",A,B);
+inicio:
+   //system("pause");
+   R=A%B; 
+   A=B; printf("R=%d\tB=%d\n",R,B);
+   B=(R==0)?B:R;
+//aveces se ouede incluir codigo de esamblador in line __asm("mov $0x0,%eax");
+   
+  // goto inicio; 
+     R=A%B; 
+   A=B; printf("R=%d\tB=%d\n",R,B);
+   B=(R==0)?B:R;  
+   
+salida:
+       //R=A and_eq B;
+       A=-1;int i;
+       for(i=31;i>=1;i--){
+       B=A>>i; 
+       printf("%d",B&1);
+       }
   system("PAUSE");	
   return 0;
 }
