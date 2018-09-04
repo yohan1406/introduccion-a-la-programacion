cd11f283d08b1669e61102985a7507c8279fc48a
yohan1406 <42361586+yohan1406@users.noreply.github.com>
2018-09-03 20:36:29 -0500

    Add files via upload

--- Practica 1_4.c
+++ Practica 1_4.c
@@ -3,17 +3,18 @@
 
 int main(int argc, char *argv[])
 {
-  int prueba, seg_tot, hr, min, seg;
-  printf("Ingrese la cantidad de segundos:\n");
-  scanf("%d", &seg_tot);
-  min=(seg_tot/60)%60;
-  prueba=(seg_tot/60)<60?1:0;
-  hr=prueba?0:(seg_tot/60)/60;
-  seg=seg_tot%60;
-  
-  printf("El resultado en horas es:%d\n", hr);
-  printf("El resultado en minutos es:%d\n", min); 
-  printf("El resultado en segundos es:%d\n", seg);
+  float F,C,c,f,ff,cc,Letra;
+  printf("Introduzca temperatura en grados:");
+  scanf("%f", &cc);
+  c=1; 
+  f=0;
+  F = cc*1.80+32;
+  C=(cc-32)/1.80;
+    Letra=(cc<0)?C:0;
+  printf("Grados C equivalentes %4.2f F\n",F, Letra);
+ Letra=(cc<1)?F:0;
+  printf("Grados F equivalentes a %4.2f C\n",C);
+ 
   system("PAUSE");	
   return 0;
 }
